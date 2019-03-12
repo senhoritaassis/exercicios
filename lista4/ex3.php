86d8193044129a4820f5218553e81a12398fa86d
1831038 <1831038@LABBMAQ19.itp.local>
2019-03-12 12:46:59 -0300

    adicionado exercicios da lista 4

---
+++ lista4/ex3.php
@@ -0,0 +1,70 @@
+<div>
+		<h1>Abra uma conta</h1>
+		<p><strong>É gratuito e sempre será.</strong></p>
+		<form method="" target="_blank" onsubmit="try {return window.confirm(&quot;Este formulário talvez não funcione corretamente devido a certas restrições de segurança.\nQuer continuar?&quot;);} catch (e) {return false;}">
+			<input type="text" name="nome">
+			<input type="text" name="sobrenome">
+			<br><br>
+			<input type="text" name="cadastro">
+			<br><br>
+			<input type="text" name="cadastro">
+			<br><br>
+			<input type="password">
+			<br><br>
+			<label><strong>Aniversário</strong></label>
+			<br><br>
+			<select name="dia">
+				<option>Dia</option>
+				<?php for($i=)
+                                ?>
+			</select>
+			<select name="mes">
+				<option>Mês</option>
+				<option>jan</option>
+				<option>fev</option>
+				<option>mar</option>
+				<option>abr</option>
+				<option>mai</option>
+				<option>jun</option>
+				<option>jul</option>
+				<option>ago</option>
+				<option>set</option>
+				<option>out</option>
+				<option>nov</option>
+				<option>dez</option>
+			</select>
+			<select name="ano">
+				<option>Ano</option>
+				<option>2016</option>
+				<option>2015</option>
+				<option>2014</option>
+				<option>2013</option>
+				<option>2012</option>
+				<option>2011</option>
+				<option>2010</option>
+				<option>2009</option>
+				<option>2008</option>
+				<option>2007</option>
+				<option>2006</option>
+				<option>2005</option>
+				<option>2004</option>
+				<option>2003</option>
+				<option>2002</option>
+				<option>2001</option>
+				<option>2000</option>
+			</select>
+			Por que preciso informar minha data de nascimento?
+			<br><br>
+			<input type="radio" name="sexo">
+			<label>Feminino</label>
+			<input type="radio" name="sexo">
+			<label>Masculino</label>
+			<br>
+			<p>Ao clicar em Abrir uma conta, você concorda com os nossos <br>
+			Termos e que leu a nossa Política de Dados, incluindo o nosso <br>
+			Uso de Cookies</p>
+			<br>
+			<button type="submit">Abrir uma conta</button>
+		</form>
+	</div>
+</body></html>
